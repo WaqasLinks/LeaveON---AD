5fd5a0e58a0815555705ee43018244c2f358254a
WaqasLinks <waqqasjavaid@gmail.com>
2021-07-27 14:19:55 +0500

    validation fiscal year implimented

--- LeaveON/Controllers/AccountController.cs
+++ LeaveON/Controllers/AccountController.cs
@@ -169,6 +169,7 @@ namespace LeaveON.Controllers
       //ADUser = "bsserviceaccount@intechww.com";
       //ADUser = "Ahsan.Ahmad@intechww.com";
       ADUser = "muzammil.riaz@intechww.com";
+      //ADUser = "Khaleel.khan@intechww.com";
       //ADUser = "Hassan.masood@intechww.com";
       //ADUser = "salman.saleem@intechww.com";
       //ADUser = "waqqasjavaid@gmail.com";
